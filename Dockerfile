4982ba3a9b591adb8d3c929c5f4970b62ecde9f1
mathieu <mathieu.keller@sda.se>
2023-09-08 22:37:08 +0200

    fix: expose port and add logs

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ USER 10001:10001
 
 WORKDIR /home/notifier/bin/
 COPY --chown=10001:10001 target/x86_64-unknown-linux-musl/release/stadtradeln .
+EXPOSE 8080
 CMD ["./stadtradeln"]
 
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use actix_web::{get, App, HttpServer, Responder, HttpResponse};
 
 #[get("/")]
 async fn hello() -> impl Responder {
+    println!("get request");
     let request_url = format!("http://192.168.178.36:8080/convert/html2image?u=doctron&p=lampnick&format=jpeg&Quality=80&url=https%3A%2F%2Fwww.stadtradeln.de%2Ffileadmin%2Fradelkalender%2Fembed%2Fradelmeter-team.php%3Fsr_team_id%3D83474%26L%3D2");
     let response = reqwest::get(&request_url).await.expect("error");
     HttpResponse::Ok().body(response.bytes().await.expect("as"))
@@ -9,6 +10,7 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("start on 8080");
     HttpServer::new(|| App::new()
         .service(hello)
     )
