65ebf0f32c1e3657e4b2286cf5fb476efe525881
mathieu <mathieu.keller@sda.se>
2023-09-08 22:49:31 +0200

    fix: scale image

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use actix_web::{get, App, HttpServer, Responder, HttpResponse};
 #[get("/")]
 async fn hello() -> impl Responder {
     println!("get request");
-    let request_url = format!("http://192.168.178.36:8080/convert/html2image?u=doctron&p=lampnick&format=jpeg&Quality=80&url=https%3A%2F%2Fwww.stadtradeln.de%2Ffileadmin%2Fradelkalender%2Fembed%2Fradelmeter-team.php%3Fsr_team_id%3D83474%26L%3D2");
+    let request_url = format!("http://192.168.178.36:8080/convert/html2image?u=doctron&p=lampnick&customClip=true&clipX=0&clipY=0&clipWidth=400&clipHeight=1500&clipScale=2&format=jpeg&Quality=80&url=https%3A%2F%2Fwww.stadtradeln.de%2Ffileadmin%2Fradelkalender%2Fembed%2Fradelmeter-team.php%3Fsr_team_id%3D83474%26L%3D2");
     let response = reqwest::get(&request_url).await.expect("error");
     HttpResponse::Ok().body(response.bytes().await.expect("as"))
 }
